aae1ef959669fed246cc398475ad74b07951c6ba
Renan Costa <renancitow@hotmail.com>
2015-07-15 22:52:40 -0300

    First commit

---
+++ src/main/java/br/fidelizy/controller/OlaMundo.java
@@ -0,0 +1,14 @@
+package br.fidelizy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OlaMundo {
+
+	@RequestMapping("/olaMundo")
+	public String execute() {
+		System.out.println("Executando a lógica com Spring MVC");
+		return "ok";
+	}
+}
